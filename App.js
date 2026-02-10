747366e08c415d7d652f2f0472556b6994d55582
gleiSUN <go5624314@gmail.com>
2026-02-10 17:03:18 -0300

    feat: adição da side bar com o botão sair

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+// imports utilizados
 import React, { useState, useEffect } from 'react';
 import { 
   View, Text, TextInput, TouchableOpacity, 
@@ -8,9 +9,14 @@ import Login from './login';
 import { stylesSair } from './src/styles/botao_sair';
 import { stylesApp} from './src/styles/stylesApp';
 import { LinearGradient } from 'expo-linear-gradient';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 
 const CHAVE_STORAGE = '@gastos_v2';
 const CHAVE_LOGIN = '@usuario_logado';
+
+const Drawer = createDrawerNavigator();
 
 const LocalSplashScreen = () => (
   <View style={{ flex: 1, backgroundColor: '#5429cc', justifyContent: 'center', alignItems: 'center' }}>
@@ -18,6 +24,100 @@ const LocalSplashScreen = () => (
     <ActivityIndicator size="large" color="#fff" style={{ marginTop: 20 }} />
   </View>
 );
+
+// função e props para a tela principal
+function TelaPrincipal({ 
+  handleLogout, 
+  lista, 
+  totalGeral, 
+  desc, 
+  setDesc, 
+  valor, 
+  setValor, 
+  handleAdicionar, 
+  handleExcluir, 
+  handleZerarTudo,
+  isHovered,
+  setIsHovered 
+}) {
+  return (
+    <LinearGradient colors={['#e0f7fa', '#f1f8e9']} style={stylesApp.container}>
+      <View style={stylesApp.container}>
+        <View style={stylesSair.header}>
+          <Text style={stylesApp.title}>WasteBuddy</Text>
+        </View>
+        
+        <View style={stylesApp.card}>
+          <TextInput 
+            style={stylesApp.input} 
+            placeholder="Descrição" 
+            value={desc} 
+            onChangeText={setDesc} 
+          />
+          <TextInput 
+            style={stylesApp.input} 
+            placeholder="Valor (R$)" 
+            keyboardType="numeric" 
+            value={valor} 
+            onChangeText={setValor} 
+          />
+          <TouchableOpacity 
+            onMouseEnter={() => setIsHovered(true)} 
+            onMouseLeave={() => setIsHovered(false)}
+            style={[stylesApp.btn, isHovered && stylesApp.btnHover]} 
+            onPress={handleAdicionar}
+          >
+            <Text style={stylesApp.btnText}>Adicionar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <FlatList
+          data={lista}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <View style={stylesApp.item}>
+              <View>
+                <Text style={stylesApp.itemDesc}>{item.descricao}</Text>
+                <Text style={stylesApp.itemValor}>R$ {Number(item.valor).toFixed(2).replace('.', ',')}</Text>
+              </View>
+              <TouchableOpacity onPress={() => handleExcluir(item.id)}>
+                <Text style={stylesApp.btnExcluir}>Excluir</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+          ListEmptyComponent={<Text style={{textAlign: 'center', marginTop: 20, color: '#969cb2'}}>Nenhum gasto registrado.</Text>}
+        />
+
+        <View style={stylesApp.footer}>
+          <View>
+            <Text style={stylesApp.footerLabel}>Total de Gastos:</Text>
+            <Text style={stylesApp.footerTotal}>R$ {totalGeral.toFixed(2).replace('.', ',')}</Text>
+          </View>
+          <TouchableOpacity onPress={handleZerarTudo} style={stylesApp.btnZerar}>
+            <Text style={stylesApp.btnZerarText}>Zerar Tudo</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </LinearGradient>
+  );
+}
+
+//side bar
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+
+      <DrawerItem
+        label="Sair da Conta"
+        labelStyle={{ color: '#e52e4d', fontWeight: 'bold' }}
+        onPress={() => {
+          props.handleLogout();
+        }}
+      />
+    </DrawerContentScrollView>
+  );
+}
 
 export default function App() {
   const [estaLogado, setEstaLogado] = useState(false);
@@ -69,14 +169,6 @@ useEffect(() => {
     setEstaLogado(false);
   };
   
-  // if (carregandoSessao) {
-  //   return (
-  //     <View style={stylesApp.center}>
-  //       <ActivityIndicator size="large" color="#5429cc" />
-  //     </View>
-  //   );
-  // }
-
   if (exibirSplash) {
     return <LocalSplashScreen />;
   }
@@ -136,59 +228,38 @@ useEffect(() => {
   const totalGeral = lista.reduce((acc, item) => acc + (Number(item.valor) || 0), 0);
 
   return (
-    <LinearGradient
-    colors={['#e0f7fa', '#f1f8e9']} 
-    style={stylesApp.container}
-  >
-    <View style={stylesApp.container}>
-     <View style={stylesSair.header}>
-        <Text style={stylesApp.title}>WasteBuddy</Text>
+    <NavigationContainer>
+     <Drawer.Navigator 
+      drawerContent={(props) => <CustomDrawerContent {...props} handleLogout={handleLogout} />}
+      screenOptions={{
+        drawerStyle: {
+        backgroundColor: '#f0f2f5',
+        width: 280,
+       },
+        headerStyle: { backgroundColor: '#633BBC' },
+        headerTintColor: '#f2f2f3',
+      }}>
         
-        <TouchableOpacity onPress={handleLogout} style={stylesSair.btnLogout}>
-          <Text style={stylesSair.btnLogoutText}>Sair</Text>
-        </TouchableOpacity>
-      </View>
-      
-      <View style={stylesApp.card}>
-        <TextInput style={stylesApp.input} placeholder="Descrição" value={desc} onChangeText={setDesc} />
-        <TextInput style={stylesApp.input} placeholder="Valor (R$)" keyboardType="numeric" value={valor} onChangeText={setValor} />
-        <TouchableOpacity 
-        onMouseEnter={() => setIsHovered(true)} 
-        onMouseLeave={() => setIsHovered(false)}
-        style={[stylesApp.btn, isHovered && stylesApp.btnHover]} 
-        onPress={handleAdicionar}
-        >
-          <Text style={stylesApp.btnText}>Adicionar</Text>
-        </TouchableOpacity>
-      </View>
-
-      <FlatList
-        data={lista}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <View style={stylesApp.item}>
-            <View>
-              <Text style={stylesApp.itemDesc}>{item.descricao}</Text>
-              <Text style={stylesApp.itemValor}>R$ {Number(item.valor).toFixed(2).replace('.', ',')}</Text>
-            </View>
-            <TouchableOpacity onPress={() => handleExcluir(item.id)}>
-              <Text style={stylesApp.btnExcluir}>Excluir</Text>
-            </TouchableOpacity>
-          </View>
+        <Drawer.Screen name="Resumo de Gastos">
+        {(props) => (
+          <TelaPrincipal 
+            {...props} 
+            lista={lista} 
+            totalGeral={totalGeral} 
+            handleLogout={handleLogout}
+            desc={desc}
+            setDesc={setDesc}
+            valor={valor}
+            setValor={setValor}
+            handleAdicionar={handleAdicionar}
+            handleExcluir={handleExcluir}
+            handleZerarTudo={handleZerarTudo}
+            isHovered={isHovered}
+            setIsHovered={setIsHovered}
+          />
         )}
-        ListEmptyComponent={<Text style={{textAlign: 'center', marginTop: 20, color: '#969cb2'}}>Nenhum gasto registrado.</Text>}
-      />
-
-      <View style={stylesApp.footer}>
-        <View>
-          <Text style={stylesApp.footerLabel}>Total de Gastos:</Text>
-          <Text style={stylesApp.footerTotal}>R$ {totalGeral.toFixed(2).replace('.', ',')}</Text>
-        </View>
-        <TouchableOpacity onPress={handleZerarTudo} style={stylesApp.btnZerar}>
-          <Text style={stylesApp.btnZerarText}>Zerar Tudo</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-    </LinearGradient>
+      </Drawer.Screen>
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
